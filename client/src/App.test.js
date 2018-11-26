530726267bf382e50b11ec2e186319318e35f623
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-26 16:38:02 +0000

    first test passing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Title from './components/title'
 import App from './App';
+import { shallow, render } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<Title />);
+  expect(wrapper.find('.title').text()).toEqual('Acebook')
 });
---
+++ client/src/components/title.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <h1 className="title">
+        Acebook
+      </h1>
+    )
+  }
+}
+
+export default Title;
